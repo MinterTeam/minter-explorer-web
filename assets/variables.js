d2ccb8156aacd9a00462a093a88f79d851ba8db8
shrpne <shrpne@gmail.com>
2018-05-25 19:41:48 +0300

    fix url

--- assets/variables.js
+++ assets/variables.js
@@ -1,4 +1,4 @@
 export const COIN_NAME = process.env.APP_ENV === 'production' ? 'BIP' : 'MNT';
 export const MAINNET_HOST = 'https://explorer.minter.network';
-export const TESTNET_HOST = 'https://explorer.beta.minter.network';
+export const TESTNET_HOST = 'https://testnet.explorer.minter.network';
 export const CURRENT_HOST = process.env.APP_ENV === 'production' ? MAINNET_HOST : TESTNET_HOST;
