193d38a4752281900db1f65fa2bb7fa9756232cc
shrpne <shrpne@gmail.com>
2019-07-17 21:29:39 +0300

    EX-172: fix back button

--- store/mutations.js
+++ store/mutations.js
@@ -1,8 +1,22 @@
 export default {
     PUSH_HISTORY: (state, historyItem) => {
+        // pushstate, clear detached chain
         state.history.push(historyItem);
+        state.detachedHistory = [];
     },
-    POP_HISTORY: (state) => {
-        state.history.pop();
+    POP_HISTORY: (state, {isForward} = {}) => {
+        if (isForward) {
+            // popstate forward
+            const detachedItem = state.detachedHistory.shift();
+            if (detachedItem) {
+                state.history.push(detachedItem);
+            }
+        } else {
+            // popstate back
+            const historyItem = state.history.pop();
+            if (historyItem) {
+                state.detachedHistory.unshift(historyItem);
+            }
+        }
     },
 };
