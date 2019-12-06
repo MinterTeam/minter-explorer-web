6ee824dcfa7a98efdf9704c40b9d1fea32e86d9b
shrpne <shrpne@gmail.com>
2019-12-06 14:11:25 +0300

    use GTM

--- nuxt.config.js
+++ nuxt.config.js
@@ -52,11 +52,7 @@ module.exports = {
     },
     plugins: [
         { src: '~/plugins/click-blur.js', ssr: false },
-        { src: '~/plugins/seo-gtag.js', ssr: false },
-        // { src: '~/plugins/seo-ym.js', ssr: false },
-        // { src: '~/plugins/seo-fb.js', ssr: false },
-        // { src: '~/plugins/seo-vk.js', ssr: false },
-        // { src: '~/plugins/seo-tw.js', ssr: false },
+        { src: '~/plugins/seo-gtm.js', ssr: false },
         { src: '~/plugins/history.js', ssr: false },
     ],
     env: Object.assign({}, processEnv, dotEnv),
