5b746f5aa627759b3cc3c2a6741cb85746014dc5
shrpne <shrpne@gmail.com>
2020-03-06 16:34:08 +0300

    update header menu

--- assets/variables.js
+++ assets/variables.js
@@ -1,6 +1,7 @@
 export const MAINNET = 'mainnet';
 export const TESTNET = 'testnet';
 export const NETWORK = process.env.APP_ENV === MAINNET ? MAINNET : TESTNET;
+export const PURPOSE = process.env.APP_PURPOSE;
 export const BASE_TITLE_NETWORK = NETWORK === MAINNET ? '' : 'Testnet ';
 export const BASE_TITLE_END = ' — Minter';
 export const BASE_TITLE = BASE_TITLE_NETWORK + 'Explorer' + BASE_TITLE_END;
@@ -9,7 +10,7 @@ export const COIN_NAME = NETWORK === MAINNET ? 'BIP' : 'MNT';
 export const CHAIN_ID = NETWORK === MAINNET ? 1 : 2;
 export const UNBOND_PERIOD = NETWORK === MAINNET ? 518400 : 518400;
 export const MAINNET_WEB_URL = 'https://explorer.minter.network';
-export const TESTNET_WEB_URL = 'https://testnet.explorer.minter.network';
+export const HISTORY_WEB_URL = 'https://v1-history.explorer.minter.network';
 export const EXPLORER_API_URL = process.env.APP_EXPLORER_API_URL;
 export const EXPLORER_RTM_URL = process.env.APP_EXPLORER_RTM_URL;
 export const GATE_API_URL = process.env.APP_GATE_API_URL;
--- components/HeaderMenu.vue
+++ components/HeaderMenu.vue
@@ -1,26 +1,29 @@
 <script>
-    import {MAINNET_WEB_URL, TESTNET_WEB_URL, NETWORK} from "~/assets/variables";
+    import {MAINNET_WEB_URL, HISTORY_WEB_URL, PURPOSE} from "~/assets/variables";
 
     export default {
         computed: {
             isMain() {
-                return NETWORK === 'mainnet';
+                return PURPOSE !== 'history';
+            },
+            isHistory() {
+                return PURPOSE === 'history';
             },
             mainnetUrl() {
                 return this.isMain ? false : MAINNET_WEB_URL;
             },
-            testnetUrl() {
-                return !this.isMain ? false : TESTNET_WEB_URL;
+            historyUrl() {
+                return this.isHistory ? false : HISTORY_WEB_URL;
             },
         },
     };
 </script>
 
 <template>
-    <div class="header__menu">
+    <div class="header__menu u-hidden-large-down">
         <a class="header__link" :href="mainnetUrl" v-if="mainnetUrl">Mainnet</a>
         <nuxt-link class="header__link is-active" to="/" v-else>Mainnet</nuxt-link>
-        <a class="header__link" :href="testnetUrl" v-if="testnetUrl">Testnet</a>
-        <nuxt-link class="header__link is-active" to="/" v-else>Testnet</nuxt-link>
+        <a class="header__link" :href="historyUrl" v-if="historyUrl">History</a>
+        <nuxt-link class="header__link is-active" to="/" v-else>History</nuxt-link>
     </div>
 </template>
